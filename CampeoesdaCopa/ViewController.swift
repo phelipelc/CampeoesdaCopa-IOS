d14dee07772ed931660aad8e4d6c46a26c493d61
Phelipe Lopes <phelipelcampos@hotmail.com>
2018-06-11 12:47:11 -0300

    Initial Commit

---
+++ CampeoesdaCopa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CampeoesdaCopa
+//
+//  Created by Phelipe Campos on 11/06/18.
+//  Copyright © 2018 Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
